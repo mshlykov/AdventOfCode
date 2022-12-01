c9f2334f31615df4bd4d0b8c9a219f1abc77c2f1
Maksym Shlykov <maksym_shlykov@ukr.net>
2022-12-01 14:30:46 +0200

    day 1

---
+++ AdventOfCode/CalorieCounting.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode
+{
+    internal class CalorieCounting
+    {
+        public static int SolveP1(string fileName) => GetSums(fileName)
+            .Max();
+
+        public static int SolveP2(string fileName) => GetSums(fileName)
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+
+        private static IEnumerable<int> GetSums(string fileName)
+        {
+            using var file = File.OpenRead(fileName);
+            using StreamReader sr = new(file);
+
+            while (!sr.EndOfStream)
+            {
+                var sum = 0;
+                while (!sr.EndOfStream && int.TryParse(sr.ReadLine(), out var currentNumber))
+                {
+                    sum += currentNumber;
+                }
+
+                yield return sum;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode;
+
+Console.WriteLine(CalorieCounting.SolveP1("CalorieCounting.txt"));
+Console.WriteLine(CalorieCounting.SolveP2("CalorieCounting.txt"));
