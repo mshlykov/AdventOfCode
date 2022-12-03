3028bdcae5ea4bcd8616594dc3b51d402ed85a4c
Maksym Shlykov <maksym_shlykov@ukr.net>
2022-12-03 11:49:38 +0200

    day 3

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,3 +5,6 @@ Console.WriteLine(CalorieCounting.SolveP2("CalorieCounting.txt"));
 
 Console.WriteLine(RockPaperScissors.SolveP1("RockPaperScissors.txt"));
 Console.WriteLine(RockPaperScissors.SolveP2("RockPaperScissors.txt"));
+
+Console.WriteLine(RucksackReorganization.SolveP1("RucksackReorganization.txt"));
+Console.WriteLine(RucksackReorganization.SolveP2("RucksackReorganization.txt"));
---
+++ AdventOfCode/RucksackReorganization.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode
+{
+    internal class RucksackReorganization
+    {
+        public static int SolveP1(string fileName) => GetRucksacks(fileName)
+            .Sum(x => x.Substring(0, x.Length / 2)
+                .ToHashSet()
+                .Intersect(x.Substring(x.Length / 2))
+                .Sum(x => GetPriority(x)));
+
+        public static int SolveP2(string fileName) => GetRucksacks(fileName)
+            .Select((item, idx) => (item, idx))
+            .GroupBy(x => x.idx / 3, x => x.item)
+            .Sum(y => y.Aggregate(default(IEnumerable<char>), (result, x) => result == null ? x.ToHashSet() : result.Intersect(x))
+            .Sum(x => GetPriority(x)));
+
+        private static int GetPriority(char item) => char.IsLower(item) ? item - 'a' + 1 : item - 'A' + 27;
+
+        private static IEnumerable<string> GetRucksacks(string fileName)
+        {
+            using var file = File.OpenRead(fileName);
+            using StreamReader sr = new(file);
+
+            while (!sr.EndOfStream)
+            {
+                yield return sr.ReadLine() ?? string.Empty;
+            }
+        }
+    }
+}
