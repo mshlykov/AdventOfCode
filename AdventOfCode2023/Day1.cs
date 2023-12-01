1dce1e483380f5bcd7b427517b1357e600f2061c
Maksym Shlykov <maksym_shlykov@ukr.net>
2023-12-01 10:51:15 +0200

    day 1

---
+++ AdventOfCode2023/Day1.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode2023
+{
+    internal class Day1
+    {
+        public static long SolveP1(string fileName)
+        {
+            return GetInput(fileName)
+                .Select(x =>
+                {
+                    var digits = x.Where(y => char.IsDigit(y)).ToArray();
+                    return (digits[0] - '0') * 10 + digits[digits.Length - 1] - '0';
+                })
+                .Sum(x => (long)x);
+        }
+
+        public static long SolveP2(string fileName)
+        {
+            var dict = new Dictionary<string, int>() {
+                { "zero", 0 },
+                { "one", 1 }, 
+                { "two", 2 }, 
+                { "three", 3 }, 
+                { "four", 4 }, 
+                { "five", 5 }, 
+                { "six", 6 }, 
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 },
+            };
+            return GetInput(fileName)
+                .Select(x =>
+                {
+                    var digits = x.Select((y,i) => 
+                    {
+                        var key = dict.Keys.FirstOrDefault(k => x.Substring(i).StartsWith(k));
+                        if (key != null) {
+                            return dict[key];
+                        }
+                        else if (char.IsDigit(y)) {
+                            return y - '0';
+                        }
+                        else return default(int?);
+                    })
+                    .Where(x => x.HasValue)
+                    .ToArray();
+                    return digits[0].Value * 10 + digits[digits.Length - 1].Value;
+                })
+                .Sum(x => (long)x);
+        }
+
+        private static IEnumerable<string> GetInput(string fileName)
+        {
+            using var file = File.OpenRead(fileName);
+            using StreamReader sr = new(file);
+
+            while (!sr.EndOfStream)
+            {
+                yield return sr.ReadLine();
+            }
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2023;
+
+Console.WriteLine(Day1.SolveP1("Trebuchet.txt"));
+Console.WriteLine(Day1.SolveP2("Trebuchet.txt"));
