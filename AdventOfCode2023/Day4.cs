9afcb1f652b43f98165e98a9ae74f5ebc066d315
Maksym Shlykov <maksym_shlykov@ukr.net>
2023-12-04 07:51:24 +0200

    day 4

---
+++ AdventOfCode2023/Day4.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2023;
+
+internal class Day4
+{
+    public static object SolveP1(string fileName) {
+        return GetInput(fileName)
+            .Select(x => {
+                var inter = x.nums.Intersect(x.winning).Count();
+                return inter != 0 ? Math.Pow(2, inter - 1) : 0;
+            })
+            .Sum();
+    }
+
+    public static object SolveP2(string fileName) {
+        var cards = GetInput(fileName)
+            .Select(x => (val: x.nums.Intersect(x.winning).Count(), mult: 1))
+            .ToArray();
+
+        for(var i = 0; i < cards.Length; ++i)
+        {
+            for(var k = 1;  k <= cards[i].val; ++k)
+            {
+                cards[i + k] = (cards[i + k].val, cards[i + k].mult + cards[i].mult);
+            }
+        }
+
+        return cards.Sum(x => x.mult);
+    }
+
+    private static IEnumerable<(HashSet<int> winning, HashSet<int> nums)> GetInput(string fileName)
+    {
+        using var file = File.OpenRead(fileName);
+        using StreamReader sr = new(file);
+
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            var a = line.Split(": ")[^1]
+                .Split(" | ")
+                .Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(y => int.Parse(y)))
+                .ToArray();
+
+            yield return (a[0].ToHashSet(), a[1].ToHashSet());
+        }
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine(Day2.SolveP1("CubeConundrum.txt"));
 Console.WriteLine(Day2.SolveP2("CubeConundrum.txt"));
 Console.WriteLine(Day3.SolveP1("GearRatios.txt"));
 Console.WriteLine(Day3.SolveP2("GearRatios.txt"));
+Console.WriteLine(Day4.SolveP1("Scratchcards.txt"));
+Console.WriteLine(Day4.SolveP2("Scratchcards.txt"));
