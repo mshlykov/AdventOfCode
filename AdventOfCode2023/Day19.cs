f5ed24fbce88f253b1eca4ed9d57a6b7e1bee5bd
Maksym Shlykov <maksym_shlykov@ukr.net>
2023-12-19 16:00:10 +0200

    minor tweaks

--- AdventOfCode2023/Day19.cs
+++ AdventOfCode2023/Day19.cs
@@ -5,7 +5,16 @@ internal class Day19
     public static object SolveP1(string fileName)
     {
         var (rules, details) = GetInput(fileName);
-        return details.Where(x => Eval(x, rules) == "A").Sum(x => x["x"] + x["m"] + x["a"] + x["s"]);
+        var dict = new Dictionary<string, (long Min, long Max)>()
+        {
+            { "x", (1, 4000) },
+            { "m", (1, 4000) },
+            { "a", (1, 4000) },
+            { "s", (1, 4000) },
+        };
+        var cons = GetConstraints("in", dict, rules);
+        return details.Where(x => cons.Any(y => x.Keys.All(z => x[z] >= y[z].Min && x[z] <= y[z].Max)))
+            .Sum(x => x.Keys.Aggregate(0L, (r, y) => r + x[y]));
     }
 
     public static object SolveP2(string fileName)
@@ -19,10 +28,8 @@ internal class Day19
             { "s", (1, 4000) },
         };
 
-        return GetConstraints("in", dict, rules).Sum(x => (x["x"].Max - x["x"].Min + 1) *
-        (x["m"].Max - x["m"].Min + 1) *
-        (x["a"].Max - x["a"].Min + 1) *
-        (x["s"].Max - x["s"].Min + 1));
+        return GetConstraints("in", dict, rules)
+            .Sum(x => x.Keys.Aggregate(1L, (r, y) => r * (x[y].Max - x[y].Min + 1)));
     }
 
     private static IEnumerable<Dictionary<string, (long Min, long Max)>> GetConstraints(string name, Dictionary<string, (long Min, long Max)> cons, Dictionary<string, List<(string, string, long?, string)>> rulesData)
@@ -46,73 +53,33 @@ internal class Day19
             if (string.IsNullOrEmpty(rule.Item1))
             {
                 res.AddRange(GetConstraints(rule.Item4, newDict, rulesData));
+                continue;
             }
-            else
+            
+            var (min, max) = consCopy[rule.Item1];
+            switch (rule.Item2)
             {
-                var (min, max) = consCopy[rule.Item1];
-                switch (rule.Item2)
-                {
-                    case "<" when rule.Item3 > min:
-                        newDict[rule.Item1] = (min, Math.Min(max, rule.Item3.Value) - 1);
-                        consCopy[rule.Item1] = (Math.Min(max, rule.Item3.Value), max);
-                        res.AddRange(GetConstraints(rule.Item4, newDict, rulesData));
-                        break;
-                    case ">" when rule.Item3 < max:
-                        newDict[rule.Item1] = (Math.Max(min, rule.Item3.Value) + 1, max);
-                        consCopy[rule.Item1] = (min, Math.Max(min, rule.Item3.Value));
-                        res.AddRange(GetConstraints(rule.Item4, newDict, rulesData));
-                        break;
-                    default:
-                        throw new Exception();
-                }
-
-                if (consCopy[rule.Item1].Min > consCopy[rule.Item1].Max)
-                {
+                case "<" when rule.Item3 > min:
+                    newDict[rule.Item1] = (min, Math.Min(max, rule.Item3.Value) - 1);
+                    consCopy[rule.Item1] = (Math.Min(max, rule.Item3.Value), max);
+                    res.AddRange(GetConstraints(rule.Item4, newDict, rulesData));
                     break;
-                }
+                case ">" when rule.Item3 < max:
+                    newDict[rule.Item1] = (Math.Max(min, rule.Item3.Value) + 1, max);
+                    consCopy[rule.Item1] = (min, Math.Max(min, rule.Item3.Value));
+                    res.AddRange(GetConstraints(rule.Item4, newDict, rulesData));
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            if (consCopy[rule.Item1].Min > consCopy[rule.Item1].Max)
+            {
+                break;
             }
         }
 
         return res;
-    }
-
-    private static string Eval(Dictionary<string, long> detail, Dictionary<string, List<(string, string, long?, string)>> rulesData)
-    {
-        var currState = "in";
-        while (currState != "A" && currState != "R")
-        {
-            var rules = rulesData[currState];
-            var newState = default(string);
-            foreach (var rule in rules)
-            {
-                newState = EvalRule(detail, rule);
-                if (!string.IsNullOrEmpty(newState))
-                {
-                    break;
-                }
-            }
-
-            currState = newState;
-        }
-
-        return currState;
-    }
-
-    private static string EvalRule(Dictionary<string, long> detail, (string, string, long?, string) rule)
-    {
-        if (string.IsNullOrEmpty(rule.Item1))
-        {
-            return rule.Item4;
-        }
-
-        var match = rule.Item2 switch
-        {
-            ">" => detail[rule.Item1] > rule.Item3.Value,
-            "<" => detail[rule.Item1] < rule.Item3.Value,
-            _ => throw new Exception()
-        };
-
-        return match ? rule.Item4 : null;
     }
 
     private static (Dictionary<string, List<(string, string, long?, string)>> Rules, List<Dictionary<string, long>> Details) GetInput(string fileName)
@@ -124,7 +91,7 @@ internal class Day19
         var rulesData = new Dictionary<string, List<(string, string, long?, string)>>();
         while (!string.IsNullOrWhiteSpace(line))
         {
-            var ruleStart = line.IndexOf("{");
+            int ruleStart = line.IndexOf('{');
             var name = line[..ruleStart];
             var rulesStrs = line[ruleStart..].Trim('{', '}').Split(",");
             var rules = new List<(string, string, long?, string)>();
