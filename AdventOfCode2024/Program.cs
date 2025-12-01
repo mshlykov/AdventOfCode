207b7a02744925087a451ced61ae40bfa31a53b4
Maksym Shlykov <maksym_shlykov@ukr.net>
2025-12-01 17:50:34 +0200

    repo rearrangement

--- AdventOfCode2024/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using AdventOfCode2024;
-
-Console.WriteLine(Day1.SolveP1("1.txt"));
-Console.WriteLine(Day1.SolveP1("input.txt"));
-Console.WriteLine(Day1.SolveP2("2.txt"));
-Console.WriteLine(Day1.SolveP2("input.txt"));
---
+++ src/AdventOfCode2024/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2024;
+
+Console.WriteLine(Day21.SolveP1("1.txt"));
+Console.WriteLine(Day21.SolveP1("input.txt"));
+Console.WriteLine(Day21.SolveP2("2.txt"));
+Console.WriteLine(Day21.SolveP2("input.txt"));
